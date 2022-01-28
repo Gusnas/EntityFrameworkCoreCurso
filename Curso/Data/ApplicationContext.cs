b77056980f1efc0af71cc79ff7f06bef0bdc608e
Gustavo Nascimento <gustavo.nascimento@fitbank.com.br>
2022-01-28 15:04:13 -0300

    Criacao das classes bases do projeto

---
+++ Curso/Data/ApplicationContext.cs
@@ -0,0 +1,39 @@
+using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEFCore.Data
+{
+     public class ApplicationContext : DbContext
+     {
+         public DbSet<Pedido> Pedidos { get; set;}
+
+         public DbSet<Cliente> Clientes { get; set;}
+
+         public DbSet<PedidoItem> PedidoItems { get; set;}
+
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         {
+             optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqlocaldb;Initial Catalog=CursoEFCore;Integrated Security=true");
+         }
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+            modelBuilder.Entity<Cliente>(p=>
+            {
+            p.ToTable("Clientes");
+            p.HasKey(p => p.Id);
+            p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+            p.Property(p => p.Telefone).HasColumnType("CHAR(11)");
+            p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
+            p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
+            p.Property(p => p.Cidade).HasColumnType("CHAR").HasMaxLength(60).IsRequired();
+            p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
+            });
+
+            modelBuilder.Entity<Produto>(p=>
+            {
+                p.ToTable("Produtos");
+            })
+         }
+     }
+}
